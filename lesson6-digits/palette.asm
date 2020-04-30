f88e36c52b69442c2ca003760f4dc408b140f8cd
Mark Schifflin <randy.schifflin@gmail.com>
2020-04-30 15:37:48 -0700

    Add scoring and a timer

---
+++ lesson6-digits/palette.asm
@@ -0,0 +1,6 @@
+bg_palette_colors:
+  .db $22,$29,$1A,$0F,  $22,$36,$17,$0F,  $22,$30,$21,$0F,  $22,$27,$17,$0F   ;;background palette
+
+sprite_palette_colors:
+  .db $22,$29,$1A,$0F,  $22,$36,$17,$0F,  $22,$30,$21,$0F,  $22,$27,$17,$0F   ;;re-use background palette
+  ;; .db $22,$1C,$15,$14,  $22,$02,$38,$3C,  $22,$1C,$15,$14,  $22,$02,$38,$3C   ;;sprite palette
